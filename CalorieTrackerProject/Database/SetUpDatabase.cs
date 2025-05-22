8166aa65dc067afd499e50889ab734172cbcb8e7
Andrew Azriel <andrew.azriel@nl.abnamro.com>
2025-05-22 15:44:44 +0200

    WIP

--- CalorieTrackerProject/Database/SetUpDatabase.cs
+++ CalorieTrackerProject/Database/SetUpDatabase.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Data.SqlClient;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,9 +7,28 @@ namespace CalorieTracker
 {
     internal class SetUpDatabase
     {
-        private static void SetupDatabase()
+        public static void setDatabase()
         {
-            // Implementation for setting up databases
+            using (var connection = DatabaseHelper.GetBase())
+            {
+                connection.Open();
+                var command = new SqlCommand("create database CalorieTracker", connection);
+                command.ExecuteNonQuery();
+            }
         }
-    }
+
+        public static void setTables()
+        {
+            using (var connection = DatabaseHelper.GetConnection())
+            {
+                connection.Open();
+                var command = new SqlCommand("create table User(\r\UserId INT IDENTITY(1,1) PRIMARY KEY,\r\nUsername varchar(50),\r\nPassword varchar(50),\r\nFirst_Name);", connection);
+                command.ExecuteNonQuery();
+                command = new SqlCommand("create table Transactions(\r\nTransactionId INT IDENTITY(1,1) PRIMARY KEY,\r\nUserId int,\r\nDate datetime,\r\nAmount int,\r\ncategoryId int\r\n);", connection);
+                command.ExecuteNonQuery();
+                command = new SqlCommand("create table Users(\r\nUserId int IDENTITY(1,1) PRIMARY KEY,\r\nUsername varchar(50),\r\nPasswordHash varchar(100)\r\n);", connection);
+                command.ExecuteNonQuery();
+
+            }
+        }
 }
